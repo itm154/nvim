6bddaf802ca9f8b33bdd7f376bbf534738d5adf5
itm154 <ashrulfahmi@gmail.com>
2025-05-23 16:59:03 +0800

    Remove transparent background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	opts = {
-		transparent_background = true,
+		transparent_background = false,
 		integrations = {
 			fzf = true,
 			gitsigns = true,
