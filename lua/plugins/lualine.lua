678a0380bb1bb0102ede36db1af0557cf939cbc8
itm154 <ashrulfahmi@gmail.com>
2025-05-27 11:51:10 +0800

    Update lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,42 +1,48 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		icons_enabled = true,
-		theme = "auto",
-		component_separators = "",
-		section_separators = "",
-		disabled_filetypes = {
-			statusline = { "neo-tree" },
-			winbar = { "neo-tree" },
-		},
-		ignore_focus = {},
-		always_divide_middle = true,
-		globalstatus = true,
-		refresh = {
-			statusline = 1000,
-			tabline = 1000,
-			winbar = 1000,
-		},
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	winbar = {},
-	inactive_winbar = {},
-	extensions = {},
+	dependencies = { "nvim-tree/nvim-web-devicons", "bwpge/lualine-pretty-path" },
+	event = "VeryLazy",
+	config = function()
+		require("lualine").setup({
+			options = {
+				component_separators = "",
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {},
+			},
+			sections = {
+				lualine_a = {
+					{ "mode", separator = { left = "", right = "" }, icon = "" },
+				},
+				lualine_b = { "pretty_path" },
+				lualine_c = {},
+				lualine_x = {
+					{
+						"diagnostics",
+						symbols = { error = " ", warn = " ", info = " ", hint = " " },
+						update_in_insert = true,
+					},
+				},
+				lualine_y = {
+					{ "branch", icon = "" },
+					{
+						"diff",
+						symbols = { added = " ", modified = " ", removed = " " },
+						colored = true,
+					},
+				},
+				lualine_z = {
+					{ "location", separator = { left = "", right = "" }, icon = "" },
+				},
+			},
+			inactive_sections = {
+				lualine_a = { "filename" },
+				lualine_b = { "pretty_path" },
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = { "location" },
+			},
+			extensions = { "trouble" },
+		})
+	end,
 }
