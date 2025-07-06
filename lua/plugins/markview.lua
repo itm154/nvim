e85d3d51a4f53bf7d20945ad9dbddcd30b67fc86
itm154 <ashrulfahmi@gmail.com>
2025-07-07 00:27:56 +0800

    Fix markview warning message

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,8 +1,12 @@
 return {
 	"OXY2DEV/markview.nvim",
 	lazy = false,
+	priority = 49,
 	ft = { "markdown" },
 	dependencies = {
 		"saghen/blink.cmp",
+	},
+	config = {
+		experimental = { check_rtp_message = false },
 	},
 }
