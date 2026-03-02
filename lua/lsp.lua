a02194a9d999db9431a612c813aa600891c0e881
itm154 <ashrulfahmi@gmail.com>
2026-03-02 13:12:32 +0800

    Add java support
    For class

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,6 +1,7 @@
 vim.lsp.enable("rust_analyzer") -- rust
-vim.lsp.enable("lua_ls") -- lua
-vim.lsp.enable("tinymist") -- typst
-vim.lsp.enable("phpactor") -- php
+vim.lsp.enable("lua_ls") -- Lua
+vim.lsp.enable("tinymist") -- Typst
+vim.lsp.enable("phpactor") -- PHP
 vim.lsp.enable("ruff") -- python
 vim.lsp.enable("basedpyright") -- python
+vim.lsp.enable("harper_ls") -- Grammar checker
