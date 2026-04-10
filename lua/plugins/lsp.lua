aecc52021a8cc3eafbd836d65d7b730e66279a8f
itm154 <ashrulfahmi@gmail.com>
2026-04-10 18:18:49 +0800

    Fix mason-lspconfig configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
-			"williamboman/mason.nvim",
+			"mason-org/mason.nvim",
 			"saghen/blink.cmp",
 		},
 		keys = {
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -10,32 +10,20 @@ return {
 		dependencies = {
 			"mason-org/mason.nvim",
 			"neovim/nvim-lspconfig",
+			"saghen/blink.cmp",
 		},
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
-					-- Core/Lua
 					"lua_ls",
-					--
-					-- -- Web Development
-					-- "html",
-					-- "cssls",
-					-- "ts_ls",
-					-- "svelte",
-					-- "tailwindcss",
-					-- "emmet_ls",
-					--
-					-- -- Systems & Scripting
-					-- "bashls",
-					-- "basedpyright",
-					-- "rust_analyzer",
-					-- "clangd",
-					--
-					-- -- Data & Config
-					-- "jsonls",
-					-- "yamlls",
-					-- "taplo",
-					-- "marksman",
+				},
+				handlers = {
+					function(server_name)
+						local capabilities = require("blink.cmp").get_lsp_capabilities()
+						require("lspconfig")[server_name].setup({
+							capabilities = capabilities,
+						})
+					end,
 				},
 			})
 		end,
