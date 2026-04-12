bc92ce982b5caaabc8b9b8497d33d3adcb88fc30
itm154 <ashrulfahmi@gmail.com>
2026-04-12 13:24:11 +0800

    Add vim api to lua_ls

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -7,6 +7,13 @@ return {
 			runtime = {
 				version = "LuaJIT",
 			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
 		},
 	},
 }
