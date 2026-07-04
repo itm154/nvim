36113c1044912ac3efb96fa735d4ceb76ad197ee
itm154 <ashrulfahmi@gmail.com>
2026-07-04 20:01:42 +0800

    Remove basedpyright, replace with ty

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,6 +2,7 @@ vim.lsp.enable("rust_analyzer") -- rust
 vim.lsp.enable("lua_ls") -- Lua
 vim.lsp.enable("tinymist") -- Typst
 vim.lsp.enable("ruff") -- python (linter)
-vim.lsp.enable("basedpyright") -- python
+-- vim.lsp.enable("basedpyright") -- python
+vim.lsp.enable("ty") --python
 
 vim.api.nvim_create_user_command("LspInfo", "checkhealth vim.lsp", {})
