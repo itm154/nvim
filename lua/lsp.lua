ada4d06c35e73ebc078961c19eb4a54124a8c9df
itm154 <ashrulfahmi@gmail.com>
2026-08-14 00:17:17 +0800

    Delete old flake

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,8 +1,11 @@
-vim.lsp.enable("lua_ls") -- Lua
-vim.lsp.enable("tinymist") -- Typst
-vim.lsp.enable("ruff") -- python (linter)
--- vim.lsp.enable("basedpyright") -- python
-vim.lsp.enable("ty") -- python
-vim.lsp.enable("nixd") -- nix
+-- Must haves
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("nixd")
+
+-- Others
+-- vim.lsp.enable("tinymist")
+-- vim.lsp.enable("ruff")
+-- vim.lsp.enable("basedpyright")
+-- vim.lsp.enable("ty")
 
 vim.api.nvim_create_user_command("LspInfo", "checkhealth vim.lsp", {})
